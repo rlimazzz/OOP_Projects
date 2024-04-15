6818a41b8c8c3e7b500a0a7bf1f916a8ed29386c
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-04-15 15:35:13 -0300

    Create Cliente.java

---
+++ Projeto1PARTE2/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+    private String nome;
+    public Cliente(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    public void setNome(String entrada) {
+
+        this.nome = entrada;
+
+    }
+    public String getNome() {
+
+        return this.nome;
+
+    }
+}
