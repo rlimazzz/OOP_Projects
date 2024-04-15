8381c1846bc09204bc29045bb95b0409b2c24ffb
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-04-15 15:35:32 -0300

    Create Item.java

---
+++ Projeto1PARTE2/Mercado/Item.java
@@ -0,0 +1,26 @@
+package Mercado;
+
+public class Item {
+    private String nome;
+    private double preco;
+    Item(String nome, double preco) {
+        this.nome = nome;
+        this.preco  = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
