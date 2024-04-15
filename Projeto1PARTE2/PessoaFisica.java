aa7f8aa459e150928eaf2bcdb89cf4a0ba73eeba
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-04-15 15:34:33 -0300

    Create PessoaFisica.java

---
+++ Projeto1PARTE2/PessoaFisica.java
@@ -0,0 +1,49 @@
+class PessoaFisica extends Cliente{
+    private String cpf;
+    public PessoaFisica(String nome , String cpf) {
+
+        super(nome);
+        this.cpf = cpf;
+
+    }
+
+    public void setCpf(String cpf) {
+
+        this.cpf = cpf;
+
+    }
+
+    public String getCpf() {
+
+        return this.cpf;
+
+    }
+
+    public String getNome1() {
+
+        return getNome();
+
+    }
+
+    public String fisicaFuncoes(int entrada) {
+
+        if(entrada == 1) {
+
+            return "Pessoa denunciada com sucesso!";
+
+        }
+        else if(entrada == 2) {
+
+            return "Pessoa bloqueada com sucesso!";
+
+        } else if(entrada == 3) {
+            
+            return "Pedido de amizade enviado com sucesso!";
+        }else{
+            
+            
+
+        }
+
+    }
+}
